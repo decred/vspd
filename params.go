49ca973452f136fa2efab42540a1612e50e450fa
jholdstock <jholdstock@decred.org>
2020-05-15 15:55:28 +0100

    Remove dcrd ports.

--- params.go
+++ params.go
@@ -6,24 +6,20 @@ import (
 
 type netParams struct {
 	*chaincfg.Params
-	DcrdRPCServerPort   string
 	WalletRPCServerPort string
 }
 
 var mainNetParams = netParams{
 	Params:              chaincfg.MainNetParams(),
-	DcrdRPCServerPort:   "9109",
-	WalletRPCServerPort: "9111",
+	WalletRPCServerPort: "9110",
 }
 
 var testNet3Params = netParams{
 	Params:              chaincfg.TestNet3Params(),
-	DcrdRPCServerPort:   "19109",
-	WalletRPCServerPort: "19111",
+	WalletRPCServerPort: "19110",
 }
 
 var simNetParams = netParams{
 	Params:              chaincfg.SimNetParams(),
-	DcrdRPCServerPort:   "19556",
-	WalletRPCServerPort: "19558",
+	WalletRPCServerPort: "19557",
 }
