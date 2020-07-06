94f9031dcb438777023654a23184756a6cad651c
jholdstock <jholdstock@decred.org>
2020-07-06 11:04:44 +0100

    Fail build if go mod tidy/download changes

--- run_tests.sh
+++ run_tests.sh
@@ -7,14 +7,28 @@ set -ex
 
 go version
 
+# run `go mod download` and `go mod tidy` and fail if the git status of
+# go.mod and/or go.sum changes
+MOD_STATUS=$(git status --porcelain go.mod go.sum)
+go mod download
+go mod tidy
+UPDATED_MOD_STATUS=$(git status --porcelain go.mod go.sum)
+if [ "$UPDATED_MOD_STATUS" != "$MOD_STATUS" ]; then
+    echo "Running `go mod tidy` modified go.mod and/or go.sum"
+    exit 1
+fi
+
+# run tests
 env GORACE="halt_on_error=1" go test -race ./...
 
+# set output format for linter
 if [[ -v CI ]]; then
     OUT_FORMAT="github-actions"
 else
     OUT_FORMAT="colored-line-number"
 fi
 
+# run linter
 golangci-lint run --disable-all --deadline=10m \
   --out-format=$OUT_FORMAT \
   --enable=gofmt \
