ff5858df1cd795b8ea1c7582bf13c33f7846ad03
Jamie Holdstock <jholdstock@decred.org>
2021-04-30 21:35:05 +0100

    Add framework for database upgrades.
    Plus a few other miscellaneous pieces which will be usedful soon:
    
    - Remove `Get` from func names `GetCookieSecret` and `GetFeeXPub`.
    - Add helpers to encode/decode integers/bytes.

--- database/addressindex.go
+++ database/addressindex.go
@@ -5,8 +5,6 @@
 package database
 
 import (
-	"encoding/binary"
-
 	bolt "go.etcd.io/bbolt"
 )
 
@@ -22,7 +20,7 @@ func (vdb *VspDatabase) GetLastAddressIndex() (uint32, error) {
 			return nil
 		}
 
-		idx = binary.LittleEndian.Uint32(idxBytes)
+		idx = bytesToUint32(idxBytes)
 
 		return nil
 	})
@@ -35,11 +33,7 @@ func (vdb *VspDatabase) GetLastAddressIndex() (uint32, error) {
 func (vdb *VspDatabase) SetLastAddressIndex(idx uint32) error {
 	err := vdb.db.Update(func(tx *bolt.Tx) error {
 		vspBkt := tx.Bucket(vspBktK)
-
-		idxBytes := make([]byte, 4)
-		binary.LittleEndian.PutUint32(idxBytes, idx)
-
-		return vspBkt.Put(lastAddressIndexK, idxBytes)
+		return vspBkt.Put(lastAddressIndexK, uint32ToBytes(idx))
 
 	})
 
