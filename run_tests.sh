2d67a35ba5fc0ee1096eaba7188dd8ae37e9f970
jholdstock <jholdstock@decred.org>
2022-03-17 12:33:55 +0000

    Use golangci-lint GitHub action.
    - Rather than manually downloading and invoking golangci-lint, use the GitHub
    action provided by the developers.
    - Configure golangci with a config file rather than passing command line args.
    This enables the same config to be used locally and on CI without introducing
    duplication. It also allows much more flexibililty in configuration than using
    CLI args alone.

--- run_tests.sh
+++ run_tests.sh
@@ -14,28 +14,5 @@ go version
 # run tests
 env GORACE="halt_on_error=1" go test -race ./...
 
-# set output format for linter
-if [[ -v CI ]]; then
-    OUT_FORMAT="github-actions"
-else
-    OUT_FORMAT="colored-line-number"
-fi
-
 # run linter
-golangci-lint run --disable-all --deadline=10m \
-  --out-format=$OUT_FORMAT \
-  --enable=gofmt \
-  --enable=revive \
-  --enable=govet \
-  --enable=gosimple \
-  --enable=unconvert \
-  --enable=ineffassign \
-  --enable=structcheck \
-  --enable=goimports \
-  --enable=misspell \
-  --enable=unparam \
-  --enable=deadcode \
-  --enable=unused \
-  --enable=errcheck \
-  --enable=asciicheck
-  
+golangci-lint run
