ee0a6fa5bbc3ae4d361697ed0a7f804b0e8dee84
jholdstock <jholdstock@decred.org>
2023-07-04 11:55:05 +0100

    Release version 1.2.1

--- version/version.go
+++ version/version.go
@@ -19,7 +19,7 @@ const semverAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrst
 const (
 	Major = 1
 	Minor = 2
-	Patch = 0
+	Patch = 1
 )
 
 // PreRelease contains the prerelease name of the application. It is a variable
