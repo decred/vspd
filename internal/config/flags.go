2edf4eb20080f6ea71bcec27d5d84ddf86dabb7c
jholdstock <jholdstock@decred.org>
2025-11-26 09:16:56 +0000

    config: Use any instead of interface{}

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 The Decred developers
+// Copyright (c) 2024-2025 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -12,7 +12,7 @@ import (
 // via command line options. Only the help option is evaluated, any invalid
 // options are ignored. The return value indicates whether the help message was
 // printed or not.
-func WriteHelp(cfg interface{}) bool {
+func WriteHelp(cfg any) bool {
 	helpOpts := flags.Options(flags.HelpFlag | flags.PrintErrors | flags.IgnoreUnknown)
 	_, err := flags.NewParser(cfg, helpOpts).Parse()
 	return flags.WroteHelp(err)
