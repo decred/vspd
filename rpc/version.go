44d4f601c449f55507f42eac6c6503bbeb578996
jholdstock <jholdstock@decred.org>
2025-12-18 11:13:33 +0000

    rpc: Check dcrd and dcrwallet binary version.
    Checks were already in place for RPC version, but this was not
    sufficient to ensure that the binaries have actually been updated. New
    checks will now enforce the binary and RPC versions of the directly
    connected dcrd and dcrwallet, and also the dcrd which is backing the
    connected dcrwallets.

--- rpc/version.go
+++ rpc/version.go
@@ -1,10 +1,41 @@
-// Copyright (c) 2020 The Decred developers
+// Copyright (c) 2020-2025 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
 package rpc
 
-import "fmt"
+import (
+	"fmt"
+
+	dcrdtypes "github.com/decred/dcrd/rpc/jsonrpc/types/v4"
+)
+
+// minimumVersions contains the minimum expected binary and API versions for
+// dcrd and dcrwallet.
+var minimumVersions = map[string]semver{
+	"dcrd":                {Major: 2, Minor: 1},
+	"dcrdjsonrpcapi":      {Major: 8, Minor: 3},
+	"dcrwallet":           {Major: 2, Minor: 1},
+	"dcrwalletjsonrpcapi": {Major: 11, Minor: 0},
+}
+
+// checkVersion returns an error if the provided key in verMap does not have
+// semver compatibility with the minimum expected versions.
+func checkVersion(verMap map[string]dcrdtypes.VersionResult, key string) error {
+	var actualV semver
+	if ver, ok := verMap[key]; ok {
+		actualV = semver{ver.Major, ver.Minor, ver.Patch}
+	} else {
+		return fmt.Errorf("version map missing key %q", key)
+	}
+
+	minimumV := minimumVersions[key]
+	if !semverCompatible(minimumV, actualV) {
+		return fmt.Errorf("incompatible %q version, expected %s got %s",
+			key, minimumV, actualV)
+	}
+	return nil
+}
 
 type semver struct {
 	Major uint32
