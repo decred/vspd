c2dfa1ac465adace877ad5e109f3d11c589e611b
jholdstock <jholdstock@decred.org>
2026-06-15 10:20:49 +0800

    Use single instance of broadcastTicket middleware.

--- internal/webapi/middleware.go
+++ internal/webapi/middleware.go
@@ -184,164 +184,166 @@ func (w *WebAPI) vspMustBeOpen(c *gin.Context) {
 // Ticket hash, ticket hex, and parent hex are parsed from the request body and
 // validated. They are broadcast to the network using SendRawTransaction if dcrd
 // is not aware of them.
-func (w *WebAPI) broadcastTicket(c *gin.Context) {
-	const funcName = "broadcastTicket"
+func (w *WebAPI) broadcastTicket() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		const funcName = "broadcastTicket"
 
-	// Read request bytes.
-	reqBytes, err := drainAndReplaceBody(c.Request)
-	if err != nil {
-		w.log.Warnf("%s: Error reading request (clientIP=%s): %v", funcName, c.ClientIP(), err)
-		w.sendErrorWithMsg(err.Error(), types.ErrBadRequest, c)
-		return
-	}
+		// Read request bytes.
+		reqBytes, err := drainAndReplaceBody(c.Request)
+		if err != nil {
+			w.log.Warnf("%s: Error reading request (clientIP=%s): %v", funcName, c.ClientIP(), err)
+			w.sendErrorWithMsg(err.Error(), types.ErrBadRequest, c)
+			return
+		}
 
-	// Parse request to ensure ticket hash/hex and parent hex are included.
-	var request struct {
-		TicketHex  string `json:"tickethex" binding:"required"`
-		TicketHash string `json:"tickethash" binding:"required"`
-		ParentHex  string `json:"parenthex" binding:"required"`
-	}
-	if err := binding.JSON.BindBody(reqBytes, &request); err != nil {
-		w.log.Warnf("%s: Bad request (clientIP=%s): %v", funcName, c.ClientIP(), err)
-		w.sendErrorWithMsg(err.Error(), types.ErrBadRequest, c)
-		return
-	}
+		// Parse request to ensure ticket hash/hex and parent hex are included.
+		var request struct {
+			TicketHex  string `json:"tickethex" binding:"required"`
+			TicketHash string `json:"tickethash" binding:"required"`
+			ParentHex  string `json:"parenthex" binding:"required"`
+		}
+		if err := binding.JSON.BindBody(reqBytes, &request); err != nil {
+			w.log.Warnf("%s: Bad request (clientIP=%s): %v", funcName, c.ClientIP(), err)
+			w.sendErrorWithMsg(err.Error(), types.ErrBadRequest, c)
+			return
+		}
 
-	// Ensure the provided ticket hex is a valid ticket.
-	msgTx, err := decodeTransaction(request.TicketHex)
-	if err != nil {
-		w.log.Errorf("%s: Failed to decode ticket hex (ticketHash=%s): %v",
-			funcName, request.TicketHash, err)
-		w.sendErrorWithMsg("cannot decode ticket hex", types.ErrBadRequest, c)
-		return
-	}
+		// Ensure the provided ticket hex is a valid ticket.
+		msgTx, err := decodeTransaction(request.TicketHex)
+		if err != nil {
+			w.log.Errorf("%s: Failed to decode ticket hex (ticketHash=%s): %v",
+				funcName, request.TicketHash, err)
+			w.sendErrorWithMsg("cannot decode ticket hex", types.ErrBadRequest, c)
+			return
+		}
 
-	err = isValidTicket(msgTx)
-	if err != nil {
-		w.log.Warnf("%s: Invalid ticket (clientIP=%s, ticketHash=%s): %v",
-			funcName, c.ClientIP(), request.TicketHash, err)
-		w.sendError(types.ErrInvalidTicket, c)
-		return
-	}
+		err = isValidTicket(msgTx)
+		if err != nil {
+			w.log.Warnf("%s: Invalid ticket (clientIP=%s, ticketHash=%s): %v",
+				funcName, c.ClientIP(), request.TicketHash, err)
+			w.sendError(types.ErrInvalidTicket, c)
+			return
+		}
 
-	// Ensure hex matches hash.
-	if msgTx.TxHash().String() != request.TicketHash {
-		w.log.Warnf("%s: Ticket hex/hash mismatch (clientIP=%s, ticketHash=%s)",
-			funcName, c.ClientIP(), request.TicketHash)
-		w.sendErrorWithMsg("ticket hex does not match hash", types.ErrBadRequest, c)
-		return
-	}
+		// Ensure hex matches hash.
+		if msgTx.TxHash().String() != request.TicketHash {
+			w.log.Warnf("%s: Ticket hex/hash mismatch (clientIP=%s, ticketHash=%s)",
+				funcName, c.ClientIP(), request.TicketHash)
+			w.sendErrorWithMsg("ticket hex does not match hash", types.ErrBadRequest, c)
+			return
+		}
 
-	// Ensure the provided parent hex is a valid tx.
-	parentTx, err := decodeTransaction(request.ParentHex)
-	if err != nil {
-		w.log.Errorf("%s: Failed to decode parent hex (ticketHash=%s): %v", funcName, request.TicketHash, err)
-		w.sendErrorWithMsg("cannot decode parent hex", types.ErrBadRequest, c)
-		return
-	}
-	parentHash := parentTx.TxHash()
+		// Ensure the provided parent hex is a valid tx.
+		parentTx, err := decodeTransaction(request.ParentHex)
+		if err != nil {
+			w.log.Errorf("%s: Failed to decode parent hex (ticketHash=%s): %v", funcName, request.TicketHash, err)
+			w.sendErrorWithMsg("cannot decode parent hex", types.ErrBadRequest, c)
+			return
+		}
+		parentHash := parentTx.TxHash()
 
-	// Check if local dcrd already knows the parent tx.
-	dcrdClient := c.MustGet(dcrdKey).(*rpc.DcrdRPC)
-	dcrdErr := c.MustGet(dcrdErrorKey)
-	if dcrdErr != nil {
-		w.log.Errorf("%s: %v", funcName, dcrdErr.(error))
-		w.sendError(types.ErrInternalError, c)
-		return
-	}
+		// Check if local dcrd already knows the parent tx.
+		dcrdClient := c.MustGet(dcrdKey).(*rpc.DcrdRPC)
+		dcrdErr := c.MustGet(dcrdErrorKey)
+		if dcrdErr != nil {
+			w.log.Errorf("%s: %v", funcName, dcrdErr.(error))
+			w.sendError(types.ErrInternalError, c)
+			return
+		}
 
-	_, err = dcrdClient.GetRawTransaction(parentHash.String())
-	if err != nil {
-		var e *wsrpc.Error
-		if errors.As(err, &e) && e.Code == rpc.ErrNoTxInfo {
-			// ErrNoTxInfo means local dcrd is not aware of the parent. We have
-			// the hex, so we can broadcast it here.
+		_, err = dcrdClient.GetRawTransaction(parentHash.String())
+		if err != nil {
+			var e *wsrpc.Error
+			if errors.As(err, &e) && e.Code == rpc.ErrNoTxInfo {
+				// ErrNoTxInfo means local dcrd is not aware of the parent. We have
+				// the hex, so we can broadcast it here.
 
-			// Before broadcasting, check that the provided parent hex is
-			// actually the parent of the ticket.
-			var found bool
-			for _, txIn := range msgTx.TxIn {
-				if !txIn.PreviousOutPoint.Hash.IsEqual(&parentHash) {
-					continue
+				// Before broadcasting, check that the provided parent hex is
+				// actually the parent of the ticket.
+				var found bool
+				for _, txIn := range msgTx.TxIn {
+					if !txIn.PreviousOutPoint.Hash.IsEqual(&parentHash) {
+						continue
+					}
+					found = true
+					break
 				}
-				found = true
-				break
-			}
 
-			if !found {
-				w.log.Errorf("%s: Invalid ticket parent (ticketHash=%s)", funcName, request.TicketHash)
-				w.sendErrorWithMsg("invalid ticket parent", types.ErrBadRequest, c)
+				if !found {
+					w.log.Errorf("%s: Invalid ticket parent (ticketHash=%s)", funcName, request.TicketHash)
+					w.sendErrorWithMsg("invalid ticket parent", types.ErrBadRequest, c)
+					return
+				}
+
+				w.log.Debugf("%s: Broadcasting parent tx %s (ticketHash=%s)", funcName, parentHash, request.TicketHash)
+				err = dcrdClient.SendRawTransaction(request.ParentHex)
+				if err != nil {
+					// Unknown output errors have special handling because they
+					// could be resolved by waiting for network propagation. Any
+					// other errors are returned to client immediately.
+					if !strings.Contains(err.Error(), rpc.ErrUnknownOutputs) {
+						w.log.Errorf("%s: dcrd.SendRawTransaction for parent tx failed (ticketHash=%s): %v",
+							funcName, request.TicketHash, err)
+						w.sendError(types.ErrCannotBroadcastTicket, c)
+						return
+					}
+
+					w.log.Debugf("%s: Parent tx references an unknown output, waiting for it in mempool (ticketHash=%s)",
+						funcName, request.TicketHash)
+
+					txBroadcast := func() bool {
+						// Wait for 1 second and try again, max 7 attempts.
+						for range 7 {
+							time.Sleep(1 * time.Second)
+							err := dcrdClient.SendRawTransaction(request.ParentHex)
+							if err == nil {
+								return true
+							}
+						}
+						return false
+					}()
+
+					if !txBroadcast {
+						w.log.Errorf("%s: Failed to broadcast parent tx, waiting didn't help (ticketHash=%s)",
+							funcName, request.TicketHash)
+						w.sendError(types.ErrCannotBroadcastTicket, c)
+						return
+					}
+				}
+
+			} else {
+				w.log.Errorf("%s: dcrd.GetRawTransaction for ticket parent failed (ticketHash=%s): %v",
+					funcName, request.TicketHash, err)
+				w.sendError(types.ErrInternalError, c)
 				return
 			}
+		}
 
-			w.log.Debugf("%s: Broadcasting parent tx %s (ticketHash=%s)", funcName, parentHash, request.TicketHash)
-			err = dcrdClient.SendRawTransaction(request.ParentHex)
+		// Check if local dcrd already knows the ticket.
+		_, err = dcrdClient.GetRawTransaction(request.TicketHash)
+		if err == nil {
+			// No error means dcrd already knows the ticket, we are done here.
+			return
+		}
+
+		// ErrNoTxInfo means local dcrd is not aware of the ticket. We have the
+		// hex, so we can broadcast it here.
+		var e *wsrpc.Error
+		if errors.As(err, &e) && e.Code == rpc.ErrNoTxInfo {
+			w.log.Debugf("%s: Broadcasting ticket (ticketHash=%s)", funcName, request.TicketHash)
+			err = dcrdClient.SendRawTransaction(request.TicketHex)
 			if err != nil {
-				// Unknown output errors have special handling because they
-				// could be resolved by waiting for network propagation. Any
-				// other errors are returned to client immediately.
-				if !strings.Contains(err.Error(), rpc.ErrUnknownOutputs) {
-					w.log.Errorf("%s: dcrd.SendRawTransaction for parent tx failed (ticketHash=%s): %v",
-						funcName, request.TicketHash, err)
-					w.sendError(types.ErrCannotBroadcastTicket, c)
-					return
-				}
-
-				w.log.Debugf("%s: Parent tx references an unknown output, waiting for it in mempool (ticketHash=%s)",
-					funcName, request.TicketHash)
-
-				txBroadcast := func() bool {
-					// Wait for 1 second and try again, max 7 attempts.
-					for range 7 {
-						time.Sleep(1 * time.Second)
-						err := dcrdClient.SendRawTransaction(request.ParentHex)
-						if err == nil {
-							return true
-						}
-					}
-					return false
-				}()
-
-				if !txBroadcast {
-					w.log.Errorf("%s: Failed to broadcast parent tx, waiting didn't help (ticketHash=%s)",
-						funcName, request.TicketHash)
-					w.sendError(types.ErrCannotBroadcastTicket, c)
-					return
-				}
+				w.log.Errorf("%s: dcrd.SendRawTransaction for ticket failed (ticketHash=%s): %v",
+					funcName, request.TicketHash, err)
+				w.sendError(types.ErrCannotBroadcastTicket, c)
+				return
 			}
-
 		} else {
-			w.log.Errorf("%s: dcrd.GetRawTransaction for ticket parent failed (ticketHash=%s): %v",
+			w.log.Errorf("%s: dcrd.GetRawTransaction for ticket failed (ticketHash=%s): %v",
 				funcName, request.TicketHash, err)
 			w.sendError(types.ErrInternalError, c)
 			return
 		}
-	}
-
-	// Check if local dcrd already knows the ticket.
-	_, err = dcrdClient.GetRawTransaction(request.TicketHash)
-	if err == nil {
-		// No error means dcrd already knows the ticket, we are done here.
-		return
-	}
-
-	// ErrNoTxInfo means local dcrd is not aware of the ticket. We have the
-	// hex, so we can broadcast it here.
-	var e *wsrpc.Error
-	if errors.As(err, &e) && e.Code == rpc.ErrNoTxInfo {
-		w.log.Debugf("%s: Broadcasting ticket (ticketHash=%s)", funcName, request.TicketHash)
-		err = dcrdClient.SendRawTransaction(request.TicketHex)
-		if err != nil {
-			w.log.Errorf("%s: dcrd.SendRawTransaction for ticket failed (ticketHash=%s): %v",
-				funcName, request.TicketHash, err)
-			w.sendError(types.ErrCannotBroadcastTicket, c)
-			return
-		}
-	} else {
-		w.log.Errorf("%s: dcrd.GetRawTransaction for ticket failed (ticketHash=%s): %v",
-			funcName, request.TicketHash, err)
-		w.sendError(types.ErrInternalError, c)
-		return
 	}
 }
 
